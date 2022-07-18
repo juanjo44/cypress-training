86e9bd10f7a72711d7b0e0f81f25d0bba33fea49
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2022-07-18 14:03:02 -0500

    Remove the comment and deleted blank spaces

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       config.defaultCommandTimeout = 20000
       config.responseTimeout = 20000
+
       // IMPORTANT return the updated config object
       return config
     }
