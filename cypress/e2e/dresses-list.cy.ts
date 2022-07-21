9d7989d9a85055d61e2dcd0449751745a64f6002
juanjo44 <38504408+juanjo44@users.noreply.github.com>
2022-07-21 15:28:06 -0500

    AAA patern (#10)
    * AAA patern
    
    * EOF was inserted and rewrite the list
    
    * the lint made put a comma in the array
    
    Co-authored-by: Juan Betancourt Osorio <juan.betancourto@perficient.com>

---
+++ cypress/e2e/dresses-list.cy.ts
@@ -0,0 +1,32 @@
+import {MenuContentPage, DressesListPage} from "../page";
+
+describe("the user navigates to the dresses page should", () => {
+  // Arrange
+  let menuContentPage: MenuContentPage;
+  let dressesListPage: DressesListPage;
+  const allDressesName = [
+    "Printed Dress",
+    "Printed Dress",
+    "Printed Summer Dress",
+    "Printed Summer Dress",
+    "Printed Chiffon Dress",
+  ];
+
+  before(() => {
+    menuContentPage = new MenuContentPage();
+    dressesListPage = new DressesListPage();
+  });
+
+  it("shows the available dresses", () =>{
+    // Arrange
+    menuContentPage.visitMenuContentPage();
+
+    // Act
+    menuContentPage.goToDressMenu();
+    dressesListPage.getDressProduct();
+
+    // Assert
+    dressesListPage.validateItemsNumber(allDressesName.length);
+    dressesListPage.validateItemsNames(allDressesName);
+  });
+});
