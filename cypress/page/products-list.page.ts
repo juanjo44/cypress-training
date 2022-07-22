b7cd8638118f3c67658be714c1f04cdfb59ff02b
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2022-07-21 19:16:36 -0500

    I worked with filter and find

--- cypress/page/products-list.page.ts
+++ cypress/page/products-list.page.ts
@@ -1,18 +1,24 @@
 class ProductsListPage {
   private addToCartButton: string;
   private checkout: string;
+  private allProducts: string;
 
   constructor (){
     this.addToCartButton = "a[title='Add to cart']";
     this.checkout = "Proceed to checkout";
+    this.allProducts = ".product_list";
   }
 
-  public addToCart(): void {
-    cy.get(this.addToCartButton).click();
+  public addToCart(productName:string): void {
+    this.findProductByName(productName).find(this.addToCartButton).click();
   }
 
   public goToShoppingCart(): void {
     cy.contains(this.checkout).click();
+  }
+
+  public findProductByName(productName:string){
+    return cy.get(this.allProducts).filter(`:contains(${productName})`);
   }
 }
 
