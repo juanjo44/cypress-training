5b4421318fd999210421b72fa91122785485582c
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2022-07-27 10:44:59 -0500

    the file is downloaded and it also exist

---
+++ cypress/page/download.page.ts
@@ -0,0 +1,23 @@
+class DownloadPage {
+  private pageURL: string;
+  private downloadButton: string;
+
+  constructor () {
+    this.pageURL = "https://demoqa.com/upload-download";
+    this.downloadButton = "#downloadButton";
+  }
+
+  public visitPage () {
+    cy.visit(this.pageURL)
+  }
+
+  public downloadImage () {
+    cy.get(this.downloadButton).click();
+  }
+
+  public getFileDownloadName(fileName: string) {
+    return cy.readFile(`cypress\\Downloads\\${fileName}`);
+  }
+}
+
+export { DownloadPage }
